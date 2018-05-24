f50c6cc884987272142350565a856478cbb84d59
Venkateshkumar S <venkateshkumarsr@gmail.com>
2018-05-24 21:37:06 +0400

    Add files via upload
    SQL FIles

---
+++ ddl.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `sai_demo`.`employee` (
+  `emp_id` INT NOT NULL,
+  `emp_name` VARCHAR(45) NULL,
+  `emp_address` VARCHAR(45) NULL,
+  `emp_dept_id` INT NULL,
+  PRIMARY KEY (`emp_id`));
+CREATE TABLE `sai_demo`.`department` (
+  `id` INT NOT NULL,
+  `department_name` VARCHAR(45) NULL,
+  `department_desc` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `sai_demo`.`employee` 
+ADD INDEX `emp_dept_id_idx` (`emp_dept_id` ASC);
+ALTER TABLE `sai_demo`.`employee` 
+ADD CONSTRAINT `emp_dept_id`
+  FOREIGN KEY (`emp_dept_id`)
+  REFERENCES `sai_demo`.`department` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE sai_demo.department` (
+id INT NOT NULL ,department_desc VARCHAR(45) NULL,department_name VARCHAR(45) NULL);
---
+++ dept_master.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sai_demo`.`department` (`id`, `department_name`, `department_desc`) VALUES ('1', 'It', 'IT');
+INSERT INTO `sai_demo`.`department` (`id`, `department_name`, `department_desc`) VALUES ('2', 'Finance', 'Finance');
+INSERT INTO `sai_demo`.`department` (`id`, `department_name`, `department_desc`) VALUES ('3', 'Operations', 'Operations');
+INSERT INTO `sai_demo`.`department` (`id`, `department_name`, `department_desc`) VALUES ('4', 'Hr', 'HR');
